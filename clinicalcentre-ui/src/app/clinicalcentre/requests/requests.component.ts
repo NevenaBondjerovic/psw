1cd7b8cabbd3ca0e9761af3fa297de7cefa340c8
Nevena <n.b.131993@gmail.com>
2019-12-07 13:52:02 +0100

    3_2_2_1 Registration requests overview page added

--- clinicalcentre-ui/src/app/clinicalcentre/requests/requests.component.ts
+++ clinicalcentre-ui/src/app/clinicalcentre/requests/requests.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestsComponent implements OnInit {
 
-  constructor() { }
+  requests;
+  selectedRequest: {};
+
+  constructor() {
+    this.requests = [
+      {userData: {username: 'user@user1', name: 'Pera', surname: 'Peric'}},
+      {userData: {username: 'user@user2', name: 'Mika', surname: 'Peric'}},
+      {userData: {username: 'user@user3', name: 'Mika', surname: 'Peric'}},
+      {userData: {username: 'user@user4', name: 'Mika', surname: 'Peric'}}
+    ];
+    this.selectedRequest = null;
+  }
 
   ngOnInit() {
+  }
+
+  onSelect(request){
+    console.log(request);
+    this.selectedRequest = request;
   }
 
 }
