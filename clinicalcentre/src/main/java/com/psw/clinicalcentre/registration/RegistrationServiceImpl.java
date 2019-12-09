bbcf9f4850017bd0e05a7121f7f1f49e2759eedd
Nevena <n.b.131993@gmail.com>
2019-12-09 22:06:32 +0100

    3_2_2_7 Sending email for accepted/rejected request is implemented

--- clinicalcentre/src/main/java/com/psw/clinicalcentre/registration/RegistrationServiceImpl.java
+++ clinicalcentre/src/main/java/com/psw/clinicalcentre/registration/RegistrationServiceImpl.java
@@ -1,23 +1,32 @@
 package com.psw.clinicalcentre.registration;
 
+import com.psw.clinicalcentre.config.EmailMessages;
 import com.psw.clinicalcentre.exceptions.AlreadyExistException;
 import com.psw.clinicalcentre.exceptions.BadRequestException;
 import com.psw.clinicalcentre.exceptions.NotFoundException;
 import com.psw.clinicalcentre.users.User;
 import com.psw.clinicalcentre.users.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static com.psw.clinicalcentre.config.EmailMessages.*;
 import static java.lang.Boolean.FALSE;
 
 @Service
 public class RegistrationServiceImpl implements RegistrationService{
 
-    private static final String USER_ALREADY_EXISTS = "Provided email already exists, please choose another one.";
+    private static final String SUBJECT = "Clinical centre registration";
+    private static final String USER_ALREADY_EXISTS_ERROR_MESSAGE = "Provided email already exists, please choose another one.";
+    private static final String NOT_FOUND_ERROR_MESSAGE = "Registration request not found.";
+    private static final String REASON_NOT_ADDED_ERROR_MESSAGE = "When the request is rejected, reason must be added. " +
+            "Please add the reason for rejecting the request.";
+    private static final String ACTIVATION_LINK = "http://localhost:4200";
 
     @Autowired
     private UserRepository userRepository;
@@ -25,11 +34,17 @@ public class RegistrationServiceImpl implements RegistrationService{
     @Autowired
     private RegistrationRepository registrationRepository;
 
+    @Autowired
+    private JavaMailSender emailSender;
+
+    @Autowired
+    private SimpleMailMessage template;
+
     @Override
     @Transactional
     public void registerUser(User user) {
         if(userRepository.findByUsername(user.getUsername()).isPresent())
-            throw new AlreadyExistException(USER_ALREADY_EXISTS);
+            throw new AlreadyExistException(USER_ALREADY_EXISTS_ERROR_MESSAGE);
 
         userRepository.save(user);
         registrationRepository.save(new RegistrationRequest(
@@ -38,21 +53,42 @@ public class RegistrationServiceImpl implements RegistrationService{
 
     @Override
     public Set<User> findUnprocessedRequests() {
-        return registrationRepository.findByProcessed(FALSE).stream().map(RegistrationRequest::getUser).collect(Collectors.toSet());
+        return registrationRepository.findByProcessed(FALSE).stream()
+                .map(RegistrationRequest::getUser).collect(Collectors.toSet());
     }
 
     @Transactional
     @Override
     public void acceptRegistrationRequest(AcceptRejectRequest request) {
-        registrationRepository.acceptRegistrationRequest(request.getUsername());
+        if(registrationRepository.findByUserUsername(request.getUsername()).isPresent()) {
+            String text = String.format(template.getText(), acceptedMessage(ACTIVATION_LINK));
+            registrationRepository.acceptRegistrationRequest(request.getUsername());
+            //sendSimpleMessage(request.getUsername(), SUBJECT, text);
+        } else {
+            throw new NotFoundException(NOT_FOUND_ERROR_MESSAGE);
+        }
     }
 
     @Transactional
     @Override
     public void rejectRegistrationRequest(AcceptRejectRequest request) {
         if(request.getDeclineReason() == null || request.getDeclineReason().isEmpty())
-            throw new BadRequestException("When the request is rejected, reason must be added. Please add the reason for rejecting the request.");
-        registrationRepository.rejectRegistrationRequest(request.getUsername(), request.getDeclineReason());
+            throw new BadRequestException(REASON_NOT_ADDED_ERROR_MESSAGE);
+        if(registrationRepository.findByUserUsername(request.getUsername()).isPresent()) {
+            String text = String.format(template.getText(), rejectedMessage(request.getDeclineReason()));
+            registrationRepository.rejectRegistrationRequest(request.getUsername(), request.getDeclineReason());
+            //sendSimpleMessage(request.getUsername(), SUBJECT, text);
+        } else {
+            throw new NotFoundException(NOT_FOUND_ERROR_MESSAGE);
+        }
+    }
+
+    private void sendSimpleMessage(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
     }
 
 }
