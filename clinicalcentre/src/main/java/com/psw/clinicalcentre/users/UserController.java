41db370d380ddc04a0318fcb576408a4e8896355
Nevena <n.b.131993@gmail.com>
2019-12-11 13:53:25 +0100

    3_2_3_2 Link in the email redirects to activation page

--- clinicalcentre/src/main/java/com/psw/clinicalcentre/users/UserController.java
+++ clinicalcentre/src/main/java/com/psw/clinicalcentre/users/UserController.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import javax.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/users")
