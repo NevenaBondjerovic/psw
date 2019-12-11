eb8d10002c323af48c0855658bdd86461a1396f5
Nevena <n.b.131993@gmail.com>
2019-12-11 11:58:01 +0100

    3_2_3_1 Activation page component added

---
+++ clinicalcentre-ui/src/app/activationpage/activationpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-activationpage',
+  templateUrl: './activationpage.component.html',
+  styleUrls: ['./activationpage.component.css']
+})
+export class ActivationpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
