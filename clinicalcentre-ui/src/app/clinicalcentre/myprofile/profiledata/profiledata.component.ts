ec4f8c704c99fe0f36f898745b7a3752fd9c5369
Nevena <n.b.131993@gmail.com>
2019-12-12 14:57:29 +0100

    3_3_1_2 Implemented getting the data from backend

--- clinicalcentre-ui/src/app/clinicalcentre/myprofile/profiledata/profiledata.component.ts
+++ clinicalcentre-ui/src/app/clinicalcentre/myprofile/profiledata/profiledata.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-profiledata',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfiledataComponent implements OnInit {
 
-  constructor() { }
+  profileData: {} = null;
+  userUrl: string = 'http://localhost:8080/users';
+  username: string = 'admin@admin.com';
+
+  errorMessage = null;
+  httpStatusInternalServerError = 500;
+  serverErrorMessage = 'Error happened while processing the data, please contact the administrator.';
+  httpStatusServerNotAvailable = 0;
+  serviceNotAvailableErrorMessage = 'The service is not available at the moment. Please try again later.';
+
+  constructor(private http: HttpClient) {
+    this.http.get(this.userUrl + '/myprofile/' + this.username)
+    .subscribe(responseData => {
+      this.profileData = responseData;
+      console.log(this.profileData);
+    }, error => {
+      this.handleError(error);
+      this.profileData = null;
+    });
+  }
 
   ngOnInit() {
   }
 
+  handleError(error){
+      if(error.status === this.httpStatusServerNotAvailable){
+        this.errorMessage = this.serviceNotAvailableErrorMessage;
+      } else if (error.status === this.httpStatusInternalServerError){
+        this.errorMessage = this.serverErrorMessage;
+      } else {
+        this.errorMessage = error.error.message;
+      }
+  }
 }
