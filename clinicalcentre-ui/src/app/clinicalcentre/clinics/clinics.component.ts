bf02743dad71b0427b66549ab7aff373d807a2f4
Nevena <n.b.131993@gmail.com>
2019-12-15 16:32:45 +0100

    3_7_11_1 Created html clinics overview page

--- clinicalcentre-ui/src/app/clinicalcentre/clinics/clinics.component.ts
+++ clinicalcentre-ui/src/app/clinicalcentre/clinics/clinics.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClinicsComponent implements OnInit {
 
-  constructor() { }
+  clinics: any[];
+  selectedClinic: {
+    id: number
+    name: string,
+    address: string,
+    score: number
+   };
+
+  constructor() {
+    this.selectedClinic = null;
+    this.clinics = [
+      { id: 1, name: "Clinic 1", address: "Some clinic address 1", score: [1,2,3,4,5]},
+      { id: 2, name: "Clinic 2", address: "Some clinic address 2", score: [1,2,3]},
+      { id: 3, name: "Clinic 3", address: "Some clinic address 3", score: [1,2,3,4]},
+      { id: 4, name: "Clinic 4", address: "Some clinic address 4", score: [1]}
+    ];
+  }
 
   ngOnInit() {
+  }
+
+  onSelect(clinic){
+    this.selectedClinic = clinic;
   }
 
 }
