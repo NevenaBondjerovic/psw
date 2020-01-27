86f6ca7180a28862fef997bf2ee8259ea17a9efe
Nevena <n.b.131993@gmail.com>
2020-01-27 20:58:55 +0100

    3_12_2_1 Created new page for appointments list

--- clinicalcentre-ui/src/app/clinicalcentre/appointments/appointments.component.ts
+++ clinicalcentre-ui/src/app/clinicalcentre/appointments/appointments.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Appointment } from 'src/app/clinicalcentre/appointments/appointment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-appointments',
@@ -7,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppointmentsComponent implements OnInit {
 
-  constructor() { }
+  appointments: [Appointment];
+  selectedAppointment: Appointment;
+  appointmentsUrl: string = 'http://localhost:8080/appointments';
+
+  loadingData: boolean = false;
+
+  errorMessage = null;
+  httpStatusInternalServerError = 500;
+  serverErrorMessage = 'Error happened while processing the data, please contact the administrator.';
+  httpStatusServerNotAvailable = 0;
+  serviceNotAvailableErrorMessage = 'The service is not available at the moment. Please try again later.';
+
+
+  constructor(private http: HttpClient) {
+    this.loadingData = true;
+    this.selectedAppointment = null;
+    this.http.get(this.appointmentsUrl)
+    .subscribe((responseData: [Appointment]) => {
+      this.appointments = responseData;
+      this.loadingData = false;
+    }, error => {
+      this.handleError(error);
+    });
+  }
 
   ngOnInit() {
+  }
+
+  handleError(error){
+      if(error.status === this.httpStatusServerNotAvailable){
+        this.errorMessage = this.serviceNotAvailableErrorMessage;
+      } else if (error.status === this.httpStatusInternalServerError){
+        this.errorMessage = this.serverErrorMessage;
+      } else {
+        this.errorMessage = error.error.message;
+      }
+  }
+
+  onSelect(appointment){
+    console.log(appointment);
   }
 
 }
