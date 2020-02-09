d57ee0d4cc8b4a0221ee69ae884fd94628ad6564
Nevena <n.b.131993@gmail.com>
2020-02-09 13:58:33 +0100

    4_1_8_3 Added firefox configuration

---
+++ clinicalcentre/selenium/src/test/java/basetests/BaseTest.java
@@ -0,0 +1,46 @@
+package basetests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public abstract class BaseTest {
+
+    private static final String CHROMEDRIVER_PATH = "src/libs/chromedriver";
+    private static final String FIREFOXDRIVER_PATH = "src/libs/geckodriver";
+
+    protected String driverType;
+    protected WebDriver driver;
+
+    @Before
+    public void setUp(){
+        if(this.driverType == "basetests"){
+            System.setProperty("webdriver.chrome.driver", CHROMEDRIVER_PATH);
+            driver = new ChromeDriver();
+        }else {
+            System.setProperty("webdriver.gecko.driver", FIREFOXDRIVER_PATH);
+            driver = new FirefoxDriver();
+        }
+    }
+
+    @After
+    public void tearDown(){
+        if(driver != null)
+            driver.quit();
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> drivers() {
+        return Arrays.asList( new Object[][] {
+                {"basetests"},
+                {"firefox"}
+        });
+    }
+
+}
---
+++ clinicalcentre/selenium/src/test/java/basetests/TopBarTest.java
@@ -0,0 +1,77 @@
+package basetests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+@RunWith(Parameterized.class)
+public class TopBarTest extends BaseTest {
+
+    private static String HOME_URL = "http://localhost:4200/clinicalcentre/home";
+    private static String LOGIN_URL = "http://localhost:4200/login";
+    private static String CLINICS_PAGE = "http://localhost:4200/clinicalcentre/clinics";
+    private static String REGISTRATION_REQUEST_PAGE = "http://localhost:4200/clinicalcentre/requests";
+    private static String WORK_CALENDAR_PAGE = "http://localhost:4200/clinicalcentre/workcalendar";
+    private static String APPOINTMENT_REQUESTS_PAGE = "http://localhost:4200/clinicalcentre/appointmentrequests";
+
+    public TopBarTest(String driverType){
+        this.driverType = driverType;
+    }
+
+
+    @Test
+    public void clinicLinkOnTheTopbar(){
+        login("n.b.131993@gmail.com", "password");
+        driver.findElement(By.id("topbar")).findElement(By.linkText("Clinics")).click();
+        WebDriverWait waitForLoad = new WebDriverWait(driver, 30);
+        waitForLoad.until(ExpectedConditions.urlToBe(CLINICS_PAGE));
+
+        Assert.assertEquals(CLINICS_PAGE, driver.getCurrentUrl());
+    }
+
+    @Test
+    public void registrationRequestsLinkOnTheTopbar(){
+        login("nevenafakultet13@gmail.com", "newpassword");
+        driver.findElement(By.id("topbar")).findElement(By.linkText("Registration requests")).click();
+        WebDriverWait waitForLoad = new WebDriverWait(driver, 30);
+        waitForLoad.until(ExpectedConditions.urlToBe(REGISTRATION_REQUEST_PAGE));
+
+        Assert.assertEquals(REGISTRATION_REQUEST_PAGE, driver.getCurrentUrl());
+    }
+
+    @Test
+    public void workCalendarLinkOnTheTopbar(){
+        login("user10@email.com", "password");
+        driver.findElement(By.id("topbar")).findElement(By.linkText("Work calendar")).click();
+        WebDriverWait waitForLoad = new WebDriverWait(driver, 30);
+        waitForLoad.until(ExpectedConditions.urlToBe(WORK_CALENDAR_PAGE));
+
+        Assert.assertEquals(WORK_CALENDAR_PAGE, driver.getCurrentUrl());
+    }
+
+    @Test
+    public void appointmentRequestsLinkOnTheTopbar(){
+        login("user10@email.com", "password");
+        driver.findElement(By.id("topbar")).findElement(By.linkText("Appointment requests")).click();
+        WebDriverWait waitForLoad = new WebDriverWait(driver, 30);
+        waitForLoad.until(ExpectedConditions.urlToBe(APPOINTMENT_REQUESTS_PAGE));
+
+        Assert.assertEquals(APPOINTMENT_REQUESTS_PAGE, driver.getCurrentUrl());
+    }
+
+    private void login(String username, String password){
+        driver.get(LOGIN_URL);
+        driver.findElement(By.name("username"))
+                .sendKeys(username);
+        driver.findElement(By.name("password"))
+                .sendKeys(password);
+        driver.findElement(By.name("submit")).click();
+        WebDriverWait waitForLoad = new WebDriverWait(driver, 30);
+        waitForLoad.until(ExpectedConditions.urlToBe(HOME_URL));
+    }
+
+}
